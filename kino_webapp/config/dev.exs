68cdaba3b528f59ed4668d3e45540bb060a89498
Jesse Trimble <jesse.trimble@livingsocial.com>
2015-12-30 10:15:10 -0700

    Use default phoenix ports, change stats and graphite ports to accomodate

--- kino_webapp/config/dev.exs
+++ kino_webapp/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :kino_webapp, KinoWebapp.Endpoint,
-  http: [port: 4002],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
